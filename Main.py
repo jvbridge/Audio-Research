81c78d86bd40ceec3dbce06a401b491dc0d7b8f9
jvbridge <jvbridge@gmail.com>
2014-05-06 17:17:00 -0700

    demo version

--- Main.py
+++ Main.py
@@ -1,30 +1,8 @@
 #!/usr/bin/python
-import pyaudio
-import wave
-import sys
+import os
+import play
+import Tkinter
 
-CHUNK = 1024
+filename = os.getcwd() + "/piano2.wav"
 
-if len(sys.argv) < 2:
-    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
-    sys.exit(-1)
-
-wf = wave.open(sys.argv[1], 'rb')
-
-p = pyaudio.PyAudio()
-
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
-data = wf.readframes(CHUNK)
-
-while data != '':
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
+play.play_file(filename)
